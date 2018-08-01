515343428f7302333015ae0538be957648d1567e
Igor <igorvasiliev7@gmail.com>
2018-08-01 22:34:09 +0300

    Merge branch 'master' of https://github.com/igorvasiliev7/java-core-grom
    # Conflicts:
    #	src/lesson12/UkrainainBankSystem.java
    #	src/lesson12/User.java

--- src/lesson12/UkrainianBankSystem.java
+++ src/lesson12/UkrainianBankSystem.java
@@ -8,41 +8,52 @@ public class UkrainianBankSystem implements BankSystem
        user.setBalance(user.getBalance()-amount-user.getBank().getCommission(amount));
     }
 
-
-
-        private void printWithdrawalErrorMsg(int amount, User user) {
-            System.out.println("Can`t withdraw amount = [" + amount + "], user = [" + user + "]");
-        }
-
-
     @Override
     public void fund(User user, int amount) {
+    if(checkLimitOfFunding(user,amount))
+        user.setBalance(user.getBalance()+amount*(1+user.getBank().getMonthlyRate()));
 //TODO
     }
 
     @Override
     public void transferMoney(User fromUser, User toUser, int amount) {
 
-        if(!checkWithdraw(fromUser, amount)) return;
+        if(!checkWithdraw(fromUser, amount)&&!checkLimitOfFunding(toUser,amount)) return;
+
 //TODO check fund
        fromUser.setBalance(fromUser.getBalance()-amount-amount*fromUser.getBank().getCommission(amount));
+        toUser.setBalance(toUser.getBalance()+amount*(1+toUser.getBank().getMonthlyRate()));
         //TODO withdraw & fund
     }
 
     @Override
     public void paySalary(User user) {
+
 //TODO
     }
 
     private boolean checkWithdrawLitim(User user, int amount, double limit){
     if(amount+user.getBank().getCommission(amount)>limit)
-    { printWithdrawalErrorMsg(amount, user); return false;}
+    { printWithdrawalErrorMsg( user,amount); return false;}
     return true;
 }
+
+    private boolean checkLimitOfFunding(User user, int amount){
+        if(user.getBank().getLimitOfFunding()-amount*(1+user.getBank().getMonthlyRate())>0){
+           printFundingErrorMsg(user,amount ); return false;}
+           return true;
+        }
+
 
     private boolean checkWithdraw(User user, int amount){
         return   checkWithdrawLitim(user,amount, user.getBank().getLimitOfWithdrawal())&&
                 checkWithdrawLitim(user,amount, user.getBalance());
 
+    }
+    private void printWithdrawalErrorMsg(User user,int amount ) {
+    System.out.println("Can`t withdraw amount = [" + amount + "], user = [" + user + "]");
+}
+    private void printFundingErrorMsg(User user,int amount ) {
+        System.out.println("Can`t fund amount = [" + amount + "], user = [" + user + "]");
     }
 }
