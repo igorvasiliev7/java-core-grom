71b9f8fedbd9d3f8ff51b1d74c46917a62e1cb48
igor <igorvasiliev7"gmail.com>
2018-08-02 14:47:31 +0300

    ss

--- src/lesson12/USBank.java
+++ src/lesson12/USBank.java
@@ -27,10 +27,10 @@ public class USBank extends Bank {
     @Override
     public double getCommission(int amount) {
         if (getCurrency() == Currency.USD) {
-            if (amount <= 1000) return amount * 0.05;
-            else return amount * 0.07;
-        } else if (amount <= 1000) return amount * 0.06;
-        else return amount * 0.08;
+            if (amount <= 1000) return  0.05;
+            else return 0.07;
+        } else if (amount <= 1000) return  0.06;
+        else return  0.08;
 
 
     }
