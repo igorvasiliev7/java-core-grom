7cb80e9b83175b84453e617d2a066bf4669206c0
igor <igorvasiliev7"gmail.com>
2018-08-03 15:20:08 +0300

    ss

--- src/lesson13/Demo.java
+++ src/lesson13/Demo.java
@@ -1,4 +1,4 @@
-package lesson9;
+package lesson13;
 
 import java.util.Arrays;
 
---
+++ src/lesson13/User.java
@@ -0,0 +1,38 @@
+package lesson13;
+
+public class User {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public User(long id, String name, String sessionId) {
+        this.id = id;
+        this.name = name;
+        this.sessionId = sessionId;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", sessionId='" + sessionId + '\'' +
+                '}';
+    }
+
+    private String name;
+    private  String sessionId;
+
+
+
+}
