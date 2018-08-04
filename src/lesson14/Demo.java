024511e4a77e84690a0b3c713d0d005d8dc94ad6
Igor <igorvasiliev7@gmail.com>
2018-08-04 09:16:10 +0300

    Merge branch 'master' of https://github.com/igorvasiliev7/java-core-grom
    # Conflicts:
    #	src/lesson12/UkrainainBankSystem.java
    #	src/lesson12/User.java

--- src/lesson14/Demo.java
+++ src/lesson14/Demo.java
@@ -17,8 +17,12 @@ public class Demo
         System.out.println(objectTest.toString());
         System.out.println(objectTest.getClass().getName());
 
-
         Object test=new Object();
+        System.out.println(test.toString());
+        System.out.println(test.getClass().getName());
         Object test2=new Object();
+        System.out.println(test2.toString());
+        System.out.println(test2.getClass().getName());
+
     }
 }
