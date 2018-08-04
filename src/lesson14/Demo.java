07c7261eb64aa5596d8db61bb1196389909c95fe
Igor <igorvasiliev7@gmail.com>
2018-08-04 09:03:18 +0300

    Merge branch 'master' of https://github.com/igorvasiliev7/java-core-grom
    # Conflicts:
    #	src/lesson12/UkrainainBankSystem.java
    #	src/lesson12/User.java

--- src/lesson14/Demo.java
+++ src/lesson14/Demo.java
@@ -4,5 +4,10 @@ public class Demo
 {
     public static void main(String[] args) {
         Object objectTest=new Object();
+        Object objectTest2=new Object();
+        objectTest.getClass().toString();
+        Object objectTest3=new Object();
+        objectTest.getClass().toString();
+
     }
 }
