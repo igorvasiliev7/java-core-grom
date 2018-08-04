718189826ade790a21e7560a3e4ebd5c66233669
Igor <igorvasiliev7@gmail.com>
2018-08-04 20:34:10 +0300

    Merge branch 'master' of https://github.com/igorvasiliev7/java-core-grom
    # Conflicts:
    #	src/lesson12/UkrainainBankSystem.java
    #	src/lesson12/User.java

--- src/lesson15/equals/Demo.java
+++ src/lesson15/equals/Demo.java
@@ -2,14 +2,13 @@ package lesson15.equals;
 
 public class Demo {
     public static void main(String[] args) {
-        File file1 = new File(11, "c:/", "doc");
-        File file2 = new File(11, "d:/", "jpg");
-        File file3 = new File(11, "c:/", "doc");
+        File file1 = new File(111, "home/Documents/test", "txt");
+        File file2 = new File(111, "d:/", "jpg");
+        File file3 = new File(111, "home/Documents/test", "txt");
 
         File someFile = file1;
 
         System.out.println(file1.equals(file2));
-
         System.out.println(file1.equals(file3));
         System.out.println(file1.hashCode());
         System.out.println(file3.hashCode());
