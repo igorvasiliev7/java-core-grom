da11d78c4545c0e6323617371deaf416f05d54b7
Igor <igorvasiliev7@gmail.com>
2018-08-04 09:20:04 +0300

    Merge branch 'master' of https://github.com/igorvasiliev7/java-core-grom
    # Conflicts:
    #	src/lesson12/UkrainainBankSystem.java
    #	src/lesson12/User.java

--- src/lesson14/Demo.java
+++ src/lesson14/Demo.java
@@ -1,28 +1,28 @@
 package lesson14;
 
-public class Demo
-{
+public class Demo {
     public static void main(String[] args) {
-        Object objectTest=new Object();
+        Object objectTest = new Object();
+
         System.out.println(objectTest.toString());
         System.out.println(objectTest.getClass().getName());
 
 
-        objectTest=new Object();
+        objectTest = new Object();
         System.out.println(objectTest.toString());
         System.out.println(objectTest.getClass().getName());
 
 
-        objectTest=new Object();
+        objectTest = new Object();
         System.out.println(objectTest.toString());
         System.out.println(objectTest.getClass().getName());
 
-        Object test=new Object();
-        System.out.println(test.toString());
-        System.out.println(test.getClass().getName());
-        Object test2=new Object();
-        System.out.println(test2.toString());
-        System.out.println(test2.getClass().getName());
+//        Object test=new Object();
+//        System.out.println(test.toString());
+//        System.out.println(test.getClass().getName());
+//        Object test2=new Object();
+//        System.out.println(test2.toString());
+//        System.out.println(test2.getClass().getName());
 
     }
 }
